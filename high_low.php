37746f1e891c8ea172895e8aa38330edfd3d013a
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-08-30 11:50:09 -0500

    Game is functioning for one round...need to think of how I can get the game to loop until the righ answer is hit.

--- high_low.php
+++ high_low.php
@@ -1,37 +1,28 @@
-<?php 
-
-// The specs for the game are:
-
-
-
-
-
-
-
-
+<?php
 // Make the game user friendly by asking the user to login. 
 
-fwrite(STDOUT, 'Welcome, What is your first name?...');
+fwrite(STDOUT, 'Welcome, What is your first name?');
 
 //User enters their first name and returns name by grabbing their input.
 
 $firstName = fgets(STDIN);
 
+
 // Welcomes the user and inputs their name in the message below.
 
-fwrite(STDOUT, "Good Day $firstname\n Let's play!");
+fwrite(STDOUT, "Good Day" . $firstName . "Let's play!" . PHP_EOL);
 
-// game picks a random number between 1 and 100.
+// game picks a $random number between 1 and 100.
 // Command to pick a random number between 1 and 100:
-// Use the following formula: int rand ( int $min , int $max )
+//Use the following formula: int rand ( int $min , int $max )
 
-$macNumber = int rand(1, 100);
+rand(1, 100);
+$rand = rand();
  
 // prompts user to guess the number
 // Game prompts user, using this format:
 
 fwrite(STDOUT, 'Please pick a number between 1 and 100.');
-$number = fgets(STDIN);
 
 // User inputs number and $guess generates their input to match with 
 // computers generated random number "int rand (1, 100)".
@@ -42,16 +33,22 @@ $guess = fgets(STDIN);
 // if user's guess is more than the number, it outputs "LOWER"
 // if a user guesses the number, the game should declare "GOOD GUESS!"
 
-if ($number < $macnumber){
-	echo "Nice Try $firstName!\n Aim Higher!";
-} elseif ($number > $macNumber) {
-	echo "Whoa $firstName!\n Aim Lower!";
+if ($guess < $rand){
+	echo "Nice Try $firstName! Aim Higher!";
+
+} elseif ($guess > $rand) {
+
+	echo "Whoa $firstName! Aim Lower!";
+
 } else {
-	echo "Right On $firstName!\n You got it!\n";
+
+	echo "Right On $firstName! You got it!";
 }
 
+
 // Asks user if they'd like to play another round
-fwrite(STDOUT, 'Want to play again $firstName?');
+fwrite(STDOUT, 'Try again $firstName!');
+
 
 
 
