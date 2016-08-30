5f379152c4c0b4431f96682c50b0d705c4cf11a9
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-08-30 15:45:59 -0500

    Update: Added a feature so that a player can set the min value and max value of the random number. Random number will generate between the two numbers the player set in gameplay.

--- high_low.php
+++ high_low.php
@@ -1,29 +1,44 @@
 <?php
-// Make the game user friendly by asking the user to login. 
+ 
+ if ($argc == 3) {
+ 	echo "arg1 is {$argv[1]} and arg2 is {$argv[2]}\n";
+ } 
+//echo 
+//1.  Make the game user friendly by asking the user to login. 
 
 fwrite(STDOUT, 'Welcome, What is your first name?');
 
-//User enters their first name and returns name by grabbing their input.
+
+//2. User enters their first name and returns name by grabbing their input.
 
 $firstName = trim(fgets(STDIN));
 
 
-// Welcomes the user and inputs their name in the message below.
+//3. Explains that the random number will start where the user sets the minimum value. 
 
-fwrite(STDOUT, "Good Day " . $firstName .  ". Let's play!" . PHP_EOL);
+fwrite(STDOUT, "Perfect " . $firstName .  ". Let's set the parameters for this game!" . $firstName . " In order for you to guess my number, set a minimum value where I can start at...");
 
-// game picks a $random number between 1 and 100.
-// Command to pick a random number between 1 and 100:
-//Use the following formula: int rand ( int $min , int $max )
+$minNumber = trim(fgets(STDIN));
 
-$rand  = rand(1, 100);
+//4. Explain that the random number stops at this number
+
+fwrite(STDOUT, "Great, Now let's set and a max value, that way I know where I can stop at.");
+
+$maxNumber = trim(fgets(STDIN));
+
+//5. Game picks a random number between parameters set in steps 3 & 4 .
+// Once numbers are set, game will begin
+// Use the following formula: int rand ( int $min , int $max )
+
+$rand  = rand($minNumber, $maxNumber);
 	echo ($rand);
 
  
-// prompts user to guess the number
-// Game prompts user, using this format:
+//6. Prompts user to guess the number
+// 		Game Play Begins
+// 		Game prompts user, using this format:
 
-fwrite(STDOUT, 'Please pick a number between 1 and 100.');
+fwrite(STDOUT, "Please pick a number between $minNumber and $maxNumber.");
 
 // User inputs number and $guess generates their input to match with 
 // computers generated random number "int rand (1, 100)".
@@ -52,9 +67,6 @@ if ($guess < $rand){
 
 // Asks user if they'd like to play another round
 fwrite(STDOUT, "Game Over $firstName!");
-
-
-
 
 
 
