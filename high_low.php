df9d4ce5569727dca62b6c92974be63c8e41a516
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-08-30 14:43:13 -0500

    Update: have the game picking a number and executing the code for intial round. Need to develop a loop that will allow you to make multiple guesses.

--- high_low.php
+++ high_low.php
@@ -5,19 +5,20 @@ fwrite(STDOUT, 'Welcome, What is your first name?');
 
 //User enters their first name and returns name by grabbing their input.
 
-$firstName = fgets(STDIN);
+$firstName = trim(fgets(STDIN));
 
 
 // Welcomes the user and inputs their name in the message below.
 
-fwrite(STDOUT, "Good Day" . $firstName . "Let's play!" . PHP_EOL);
+fwrite(STDOUT, "Good Day " . $firstName .  ". Let's play!" . PHP_EOL);
 
 // game picks a $random number between 1 and 100.
 // Command to pick a random number between 1 and 100:
 //Use the following formula: int rand ( int $min , int $max )
 
-rand(1, 100);
-$rand = rand();
+$rand  = rand(1, 100);
+	echo ($rand);
+
  
 // prompts user to guess the number
 // Game prompts user, using this format:
@@ -27,7 +28,9 @@ fwrite(STDOUT, 'Please pick a number between 1 and 100.');
 // User inputs number and $guess generates their input to match with 
 // computers generated random number "int rand (1, 100)".
 
-$guess = fgets(STDIN);
+$guess = trim(fgets(STDIN));
+
+
 
 // if user's guess is less than the number, it outputs "HIGHER"
 // if user's guess is more than the number, it outputs "LOWER"
@@ -43,11 +46,13 @@ if ($guess < $rand){
 } else {
 
 	echo "Right On $firstName! You got it!";
+
 }
 
 
 // Asks user if they'd like to play another round
-fwrite(STDOUT, 'Try again $firstName!');
+fwrite(STDOUT, "Game Over $firstName!");
+
 
 
 
